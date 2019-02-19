de624956aef5823e467408f455f171a01a7cf63c
Carlos Medina <cmedina.developer@gmail.com>
2019-02-19 00:04:53 -0400

    Link App with Store

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { reduxifyNavigator } from 'react-navigation-redux-helpers';
+import createStore from './Config/createStore';
+import AppNavigation from './Navigation/appNavigation';
+
+const { store, persistor } = createStore();
+
+const AppNav = reduxifyNavigator(AppNavigation, 'root');
+const mapStateToProps = state => ({
+  state: state.nav,
+});
+const AppWithNavigationState = connect(mapStateToProps)(AppNav);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppWithNavigationState />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
+
+export default App;
