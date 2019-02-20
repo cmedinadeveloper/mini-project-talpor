1cf47ac499b60f2a4ddec15c5cd20d9db1aee036
Carlos Medina <cmedina.developer@gmail.com>
2019-02-19 22:32:43 -0400

    Add Login Reducer

---
+++ src/Ducks/LoginReducer/LoginReducer.js
@@ -0,0 +1,62 @@
+// Constants
+export const ACTIONS = {
+  LOGIN: 'login/UPDATE',
+  UPDATE: 'login/UPDATE',
+  CLEAR: 'login/CLEAR',
+  LOGIN_ERROR: 'login/LOGIN_ERROR',
+  LOGIN_SUCCESS: 'login/LOGIN_SUCCESS',
+};
+
+// Actions
+export const updateLogin = payload => ({
+  type: ACTIONS.UPDATE,
+  payload,
+});
+
+export const clearLogin = () => ({
+  type: ACTIONS.CLEAR,
+});
+
+const initialState = {
+  success: false,
+  email: '',
+  password: '',
+  token: '',
+  errors: { emailError: '', passwordError: '' },
+  loading: false,
+};
+
+const LoginReducer = (state = initialState, action) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case ACTIONS.LOGIN_SUCCESS: {
+      return {
+        ...initialState,
+        success: true,
+      };
+    }
+    case ACTIONS.LOGIN_ERROR: {
+      return {
+        success: false,
+        error: payload,
+      };
+    }
+    case ACTIONS.UPDATE: {
+      return {
+        ...state,
+        ...payload,
+      };
+    }
+    case ACTIONS.CLEAR: {
+      return {
+        ...initialState,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default LoginReducer;
