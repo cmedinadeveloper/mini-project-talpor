dcbbae8866126896cf8cfc58789e7ea966280621
Carlos Medina <cmedina.developer@gmail.com>
2019-02-21 17:10:35 -0400

    Finish Sign Up

--- src/Screens/SignUp/SignUpScreen.js
+++ src/Screens/SignUp/SignUpScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
-import { Button, Text, Form, Item, Input } from 'native-base';
+import { Button, Text, Form, Item, Input, Spinner } from 'native-base';
+import Firebase from 'react-native-firebase';
+import ToastMessage from '../../Components/ToastMessage/ToastMessage';
 import { updateLogin } from '../../Ducks/LoginReducer/LoginReducer';
 import MainContainer from '../../Containers/MainContainer/MainContainer';
 import InputField from '../../Components/InputField/InputField';
@@ -9,6 +11,54 @@ import validate from '../../Utils/ValidationWrapper';
 import styles from './styles';
 
 class SignUpScreen extends Component {
+  signIn() {
+    const {
+      email,
+      password,
+      updateLogin,
+      navigation,
+      displayName,
+      loading,
+    } = this.props;
+
+    const emailError = validate('email', email);
+    const passwordError = validate('password', password);
+    updateLogin({
+      errors: {
+        emailError,
+        passwordError,
+      },
+    });
+
+    if (!emailError && !passwordError && loading === false) {
+      const values = {
+        email,
+        password,
+      };
+      updateLogin({ loading: true });
+      Firebase.auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => {
+          if (user) {
+            Firebase.auth()
+              .currentUser.updateProfile({
+                displayName,
+              })
+              .then(() => {
+                navigation.navigate('App');
+                updateLogin({ loading: false });
+              });
+          }
+        })
+        .catch(error => {
+          const { code, message } = error;
+          console.log(`Error code ${code}, ${message}`);
+          updateLogin({ loading: false });
+          ToastMessage(message);
+        });
+    }
+  }
+
   render() {
     const {
       email,
@@ -19,60 +69,69 @@ class SignUpScreen extends Component {
       displayName,
       displayNameError,
       navigation,
+      loading,
     } = this.props;
 
     return (
       <MainContainer navigation={navigation} backButton viewTitle="Sign Up">
         <View>
-          <Form>
-            <InputField
-              onChangeText={value => {
-                updateLogin({ displayName: value });
+          <Form style={styles.form_container}>
+            <View>
+              <InputField
+                onChangeText={value => {
+                  updateLogin({ displayName: value });
+                }}
+                onBlur={() => {
+                  updateLogin({
+                    errors: {
+                      displayNameError: validate('displayName', displayName),
+                    },
+                  });
+                }}
+                error={displayNameError}
+                style={styles.form_input}
+                type="text"
+                placeholder="Display Name"
+              />
+              <InputField
+                onChangeText={value => {
+                  updateLogin({ email: value });
+                }}
+                onBlur={() => {
+                  updateLogin({
+                    errors: {
+                      emailError: validate('email', email),
+                    },
+                  });
+                }}
+                error={emailError}
+                style={styles.form_input}
+                type="email"
+                placeholder="Email"
+              />
+              <InputField
+                onChangeText={value => updateLogin({ password: value })}
+                onBlur={() => {
+                  updateLogin({
+                    errors: {
+                      passwordError: validate('password', password),
+                    },
+                  });
+                }}
+                error={passwordError}
+                style={styles.form_input}
+                type="password"
+                placeholder="Password"
+              />
+            </View>
+            <Button
+              onPress={() => {
+                this.signIn();
               }}
-              onBlur={() => {
-                updateLogin({
-                  errors: {
-                    displayNameError: validate('displayName', displayName),
-                  },
-                });
-              }}
-              error={displayNameError}
-              style={styles.form_input}
-              type="text"
-              placeholder="Display Name"
-            />
-            <InputField
-              onChangeText={value => {
-                updateLogin({ email: value });
-              }}
-              onBlur={() => {
-                updateLogin({
-                  errors: {
-                    emailError: validate('email', email),
-                  },
-                });
-              }}
-              error={emailError}
-              style={styles.form_input}
-              type="email"
-              placeholder="Email"
-            />
-            <InputField
-              onChangeText={value => updateLogin({ password: value })}
-              onBlur={() => {
-                updateLogin({
-                  errors: {
-                    passwordError: validate('password', password),
-                  },
-                });
-              }}
-              error={passwordError}
-              style={styles.form_input}
-              type="password"
-              placeholder="Password"
-            />
-            <Button style={styles.form_button} block>
-              <Text>Create Account</Text>
+              style={styles.form_button}
+              block
+            >
+              {loading === true ? <Spinner /> : <Text>Create Account</Text>}
             </Button>
           </Form>
         </View>
